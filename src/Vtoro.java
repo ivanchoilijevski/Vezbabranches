bff3de1e35d12d5120b62afefe39255b3e1da7a6
ivanchoilijevski <ilijevskii@gmail.com>
2022-02-10 21:27:26 +0100

    My first commit

---
+++ src/Vtoro.java
@@ -0,0 +1,2 @@
+public class Vtoro {
+}
---
+++ src/com/company/Helloworld.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Helloworld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        sayHello (name: "Ivancho")
+        sayGoodbye (name: "Ivancho");
+    }
+
+    public Helloworld(int age) {
+        this.age = age;
+    }
+
+    private int age = 5;
+}
